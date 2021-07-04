c7b46d98567ac532bf62c2b92e86ab962544623c
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-07-04 16:14:46 -0300

    add connect with db

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.DB_CONNECTED, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log(">>> DB connected <<<") }
+).catch(error => { console.log(error) })
 
 app.listen(process.env.PORT, () => {
     console.log(`>>> Server running on port ${process.env.PORT} <<<`)
