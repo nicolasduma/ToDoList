68fba1d6ea20e2ab00676aa6ab767976e9833320
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-07-09 19:09:32 -0300

    update interface animations

--- client/public/assets/js/interface.js
+++ client/public/assets/js/interface.js
@@ -1,15 +1,13 @@
 let usedThame = localStorage.getItem("thame") ? localStorage.getItem("thame") == "true" : true
 setThame(usedThame)
 
-document.addEventListener("DOMContentLoaded", () => {
-    let btnThame = document.querySelector("#change-thame")
-
-
-    btnThame.addEventListener("click", () => {
+    document.querySelector("#change-thame").addEventListener("click", () => {
         usedThame = !usedThame
         setThame(usedThame)
         localStorage.setItem("thame", usedThame.toString())
     })
+
+    document.querySelector("#description-new-task").addEventListener("keyup", keyOfNewDescInput)
 })
 
 function setThame(usedThame) {
@@ -19,4 +17,13 @@ function setThame(usedThame) {
         element.classList.remove(thames[1])
         element.classList.add(thames[0])
     })
+}
+
+function keyOfNewDescInput(event) {
+    if (event.target.value != "") updateClasses = ["opacity-0", "opacity-100"]
+    else if (event.target.value == "") updateClasses = ["opacity-100", "opacity-0"]
+
+    let element = document.querySelector("#actions-form")
+    element.classList.remove(updateClasses[0])
+    element.classList.add(updateClasses[1])
 }
