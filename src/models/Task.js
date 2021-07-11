263c20cf7773b75f611d5b44c285fae5ec129ff8
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-07-11 16:01:53 -0300

    update Task.js

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 
 const taskSchema = mongoose.Schema({
-    title: {type: String, minlength: 1, maxlength: 35},
-    description: {type: String, minlength: 1, riquired: true},
-    category: {type: Boolean, default: true, required: true} // true = Personal & false = Work
+    description: {type: String, minlength: 1, maxlength: 50, riquired: true},
+    category: {type: Boolean, default: true, required: true}, // true = Personal & false = Work
+    done: {type: Boolean, default: false, required: true},
 })
 
 module.exports = mongoose.model("Task", taskSchema)
