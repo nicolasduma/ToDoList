dd9ed5f11cb5cb383fd726540bbf26a94d07584e
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-07-14 15:35:59 -0300

    treat errors

--- client/public/assets/js/interface.js
+++ client/public/assets/js/interface.js
@@ -1,5 +1,6 @@
+let usedThame = localStorage.getItem("thame") ? localStorage.getItem("thame") == "true" : true
+
 document.addEventListener("DOMContentLoaded", () => {
-    let usedThame = localStorage.getItem("thame") ? localStorage.getItem("thame") == "true" : true
     setThame(usedThame)
 
     document.querySelector("#change-thame").addEventListener("click", () => {
@@ -37,4 +38,23 @@ function keyOfNewDescInput(event) {
     let element = document.querySelector("#actions-form")
     element.classList.remove(updateClasses[0])
     element.classList.add(updateClasses[1])
+}
+
+function updateScreenWidthNewTask(task) {
+    let thame = usedThame ? "light" : "dark"
+    let newTaskElement = `
+    <li id="${task._id}" class="continer-task d-flex align-items-center bg-primary ft-color-secondary ${thame}">
+        <div class="conteiner-task-completed d-flex justify-content-center align-items-center">
+            <input type="checkbox" data-done="${task.done}" onchange="changeDoneInput(this, '${task._id})" id="checkbox${task._id}" class="task-completed d-none category-${task.category} ${thame}">
+            <label for="checkbox${task._id}"></label>
+        </div>
+
+        <p class="description-task">
+            ${task.description}
+        </p>
+        <button onclick="deleteTask('${task._id}')" aria-label="Delete" class="btn-delete hover-tooltip ${thame}"><i class="uil uil-trash ft-color-secondary ${thame}"></i></button>
+    </li>
+    `
+
+    document.getElementById("content-all-tasks").innerHTML = newTaskElement + document.getElementById("content-all-tasks").innerHTML
 }
--- client/public/assets/js/taskFunctions.js
+++ client/public/assets/js/taskFunctions.js
@@ -18,7 +18,6 @@ function deleteTask(id) {
     fetch("delete/" + id, {method: "DELETE"}).then(res => {
 
         res.text().then(id => {
-            console.log(id)
             document.getElementById(id).remove()
         })
         
@@ -27,4 +26,41 @@ function deleteTask(id) {
         console.log(error)
 
     })
+}
+
+addNewTask = async () => {
+    event.preventDefault()
+    
+    let task = {
+        description: document.querySelector("#description-new-task").value.trim(),
+        category: document.querySelector("#category-new-task").value.trim(),
+    }
+
+    if ((task.description && task.description != "") && (task.category == "true" || task.category == "false")) sendRequest(task)
+    else alert("Error: undefined datas!")
+}
+
+function sendRequest(body) {
+    let request = new XMLHttpRequest()
+    request.open("POST", "http://192.168.0.108:7000/task/add", true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload = () => { 
+        let res = JSON.parse(request.responseText)
+
+        if (request.status === 200) updateScreenWidthNewTask(res)
+
+        else if (request.status === 400) {
+            alert(`Error: couldn't save task!\n${res.error.message}`)
+            console.log(res.error.message)
+            console.log(res.body)
+
+            document.querySelector("#description-new-task").value = res.body.description
+            for (let element of document.querySelector("#category-new-task").children) {
+                if (element.value == res.body.category) element.setAttribute("selected", "selected")
+                else element.removeAttribute("selected")
+            }
+        }
+    }
 }
