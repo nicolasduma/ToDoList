28fa9ef8a33fbdf996f21eb88bfe5fd2de0f7228
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-07-15 17:30:30 -0300

    update Task.js

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const taskSchema = mongoose.Schema({
-    description: {type: String, minlength: 1, maxlength: 50, riquired: true},
+    description: {type: String, minlength: 1, maxlength: 30, riquired: true},
     category: {type: Boolean, default: true, required: true}, // true = Personal & false = Work
     done: {type: Boolean, default: false, required: true},
 })
